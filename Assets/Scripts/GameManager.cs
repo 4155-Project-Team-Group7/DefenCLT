19ec3bc760a183f706c01ae87c22a0f655f0d384
Sang Vang <vangsang2019@gmail.com>
2025-04-22 09:03:57 -0400

    Updating GameManager Logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,28 +5,25 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
-    // Gameplay state
+    [Header("Gameplay State")]
     public int currentWave { get; private set; } = 1;
     public int playerHealth { get; private set; } = 10;
 
-    public Turret Turret {get; private set; } // Reference to the Turret script
-
-    // public int Map = 2
-
-    // Placed turrets tracking
+    [Header("Turret Tracking")]
+    [SerializeField] private List<GameObject> turretPrefabs; // Assign in Inspector
     private List<TurretData> placedTurrets = new List<TurretData>();
 
     private void Awake()
     {
-        if (instance = null){
+        if (instance == null)
+        {
             instance = this;
-            DontDestroyOnLoad(gameObject); // Optional: Persist across scenes
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
             Destroy(gameObject);
         }
-
     }
 
     public void SetCurrentWave(int wave)
@@ -41,14 +38,16 @@ public class GameManager : MonoBehaviour
 
     public void RegisterTurret(Vector2 position, string turretType, int upgradeLevel)
     {
+        GameObject prefab = turretPrefabs.Find(t => t.name == turretType);
+
         placedTurrets.Add(new TurretData
         {
             position = position,
             turretType = turretType,
-            upgradeLevel = upgradeLevel
+            upgradeLevel = upgradeLevel,
+            turretPrefab = prefab
         });
     }
-
 
     public List<TurretData> GetPlacedTurrets()
     {
@@ -60,8 +59,30 @@ public class GameManager : MonoBehaviour
         placedTurrets.Clear();
     }
 
+    public void LoadTurrets(TurretData[] savedTurrets)
+    {
+        ClearTurrets();
 
-    
+        foreach (var turretData in savedTurrets)
+        {
+            GameObject prefab = turretPrefabs.Find(t => t.name == turretData.turretType);
+            if (prefab != null)
+            {
+                GameObject turretGO = Instantiate(prefab, turretData.position, Quaternion.identity);
+                Turret turretScript = turretGO.GetComponent<Turret>();
+                turretScript.SetUpgradeLevel(turretData.upgradeLevel);
+            }
 
+            placedTurrets.Add(turretData);
+        }
+    }
+}
 
+[System.Serializable]
+public struct TurretData
+{
+    public Vector2 position;
+    public string turretType;
+    public int upgradeLevel;
+    public GameObject turretPrefab;
 }
